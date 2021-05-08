e2bfb07f731e4f7178c3d8258cde583d33aaa6cf
iruizdorronsoro <iruizdorronsoro@gmail.com>
2021-05-08 12:33:41 +0200

    add user model

---
+++ src/models/user.ts
@@ -0,0 +1,43 @@
+import {model, Schema, Document} from "mongoose";
+import bcrypt from 'bcrypt'
+
+
+export interface IUser extends Document {
+    email: string,
+    password: string
+
+    comparePassword: (password:string) => Promise<boolean>
+}
+
+const userSchema = new Schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+},
+    {
+        timestamps: true,
+        versionKey: false
+    })
+
+userSchema.pre<IUser>('save', async function (next) {
+    if(!this.isModified('password')) {
+        return next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const encrypt = await bcrypt.hash(this.password, salt)
+    this.password = encrypt
+})
+
+userSchema.methods.comparePassword = function (password:string):Promise<boolean> {
+    return bcrypt.compare(password, this.password)
+}
+
+export default model('User', userSchema)
